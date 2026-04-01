5c14cf12c458d8a20db8892929b151364b90e4d7
lucas <lucgom99@gmail.com>
2026-04-01 17:24:39 -0300

    update(blog): Update version installer of molab printer setup

--- src/components/blog/BlogDetail.tsx
+++ src/components/blog/BlogDetail.tsx
@@ -151,8 +151,8 @@ const BlogDetail = ({ blog, relatedBlogs = [] }: BlogDetailProps) => {
                                     Descargá el instalador de MolabPrinter para comenzar con la instalación.
                                 </p>
                                 <a 
-                                    href="/resources/Molab_Printer_Setup.exe"
-                                    download="Molab_Printer_Setup.exe"
+                                    href="/resources/Molab_Printer_Setup_1.5.0.exe"
+                                    download="Molab_Printer_Setup_1.5.0.exe"
                                     className="btn btn-theme blog-download-btn"
                                 >
                                     <i className="fas fa-download" /> Descargar Instalador

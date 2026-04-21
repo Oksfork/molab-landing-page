66d1492ab4cd068de54269c6d8d4cf5e75d42e55
lucas <lucgom99@gmail.com>
2026-04-21 09:13:18 -0300

    update(Blog): change url of print builder

--- src/components/blog/BlogDetail.tsx
+++ src/components/blog/BlogDetail.tsx
@@ -151,8 +151,8 @@ const BlogDetail = ({ blog, relatedBlogs = [] }: BlogDetailProps) => {
                                     Descargá el instalador de MolabPrinter para comenzar con la instalación.
                                 </p>
                                 <a 
-                                    href="https://github.com/Oksfork/nodejs-escpos-printer/releases/download/v1.6.1/Molab_Printer_Setup_1.6.1.exe"
-                                    download="Molab_Printer_Setup_1.6.1.exe"
+                                    href="https://github.com/Oksfork/nodejs-escpos-printer/releases/download/v1.6.2/Molab_Printer_Setup_1.6.2.exe"
+                                    download="Molab_Printer_Setup_1.6.2.exe"
                                     className="btn btn-theme blog-download-btn"
                                 >
                                     <i className="fas fa-download" /> Descargar Instalador
